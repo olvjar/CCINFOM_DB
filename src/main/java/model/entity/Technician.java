076a578782f9dba68752f0e0fead4ca44637b42e
EUGO575 <eugo_p_jacela@dlsu.edu.ph>
2025-03-23 20:53:43 +0800

    Update Technician.java
    added getters

--- src/main/java/model/entity/Technician.java
+++ src/main/java/model/entity/Technician.java
@@ -1,5 +1,37 @@
 package model.entity;
 
 public class Technician {
-    // TODO: Implement Technician model
-} 
+    private int technicianID;
+    private String technicianName;
+    private String contactNumber;
+    private String completeAddress;
+    private boolean availability;
+
+    public Technician(int technicianID, String technicianName, String contactNumber, String completeAddress, boolean availability) {
+        this.technicianID = technicianID;
+        this.technicianName = technicianName;
+        this.contactNumber = contactNumber;
+        this.completeAddress = completeAddress;
+        this.availability = availability;
+    }
+
+    public int getTechnicianID() {
+        return technicianID;
+    }
+
+    public String getTechnicianName() {
+        return technicianName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getCompleteAddress() {
+        return completeAddress;
+    }
+
+    public boolean isAvailable() {
+        return availability;
+    }
+}
