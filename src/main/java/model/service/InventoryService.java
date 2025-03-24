7077b9282899ac40db448dff51f322b24192579b
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-03-24 15:33:53 +0800

    Update InventoryService.java

--- src/main/java/model/service/InventoryService.java
+++ src/main/java/model/service/InventoryService.java
@@ -1,5 +1,30 @@
 package model.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.entity.Inventory;
+
 public class InventoryService {
-    // TODO
+    private List<Inventory> inventoryList = new ArrayList<>();
+
+    public void addInventoryItem(Inventory item) {
+        inventoryList.add(item);
+    }
+
+    public void updateInventoryItem(String productCode, int newQuantity) {
+        for (Inventory item : inventoryList) {
+            if (item.getProductCode().equals(productCode)) {
+                item.setQuantityInStock(newQuantity);
+                break;
+            }
+        }
+    }
+
+    public void deleteInventoryItem(String productCode) {
+        inventoryList.removeIf(item -> item.getProductCode().equals(productCode));
+    }
+
+    public List<Inventory> getAllInventoryItems() {
+        return inventoryList;
+    }
 }
