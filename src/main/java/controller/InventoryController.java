9483f37bf0394a5c1e0391cda9289018648a85c4
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-03-24 15:45:55 +0800

    Update InventoryController.java

--- src/main/java/controller/InventoryController.java
+++ src/main/java/controller/InventoryController.java
@@ -1,3 +1,26 @@
+package controller;
+
+import model.entity.Inventory;
+import model.service.InventoryService;
+import java.util.List;
+
 public class InventoryController {
-    // TODO
-}   
+    private InventoryService inventoryService = new InventoryService();
+
+    public void addInventory(String productCode, String productName, int quantity, String status) {
+        Inventory item = new Inventory(productCode, productName, quantity, status);
+        inventoryService.addInventoryItem(item);
+    }
+
+    public void updateInventory(String productCode, int newQuantity) {
+        inventoryService.updateInventoryItem(productCode, newQuantity);
+    }
+
+    public void deleteInventory(String productCode) {
+        inventoryService.deleteInventoryItem(productCode);
+    }
+
+    public List<Inventory> viewInventory() {
+        return inventoryService.getAllInventoryItems();
+    }
+}
